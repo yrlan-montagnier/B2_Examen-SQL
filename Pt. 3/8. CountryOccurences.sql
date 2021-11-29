d8ee672dc877b04a29d23e9777d9f7340fcaa023
Yrlan <71253160+yrlan-montagnier@users.noreply.github.com>
2021-11-29 11:30:46 +0100

    Modif exo 8

--- Pt. 3/8. CountryOccurences.sql
+++ Pt. 3/8. CountryOccurences.sql
@@ -1,13 +1,14 @@
-SELECT Country, FROM "Employee"
-UNION
-SELECT ShipCountry FROM "Order"
-UNION
-SELECT Country FROM "Customer"
+SELECT DISTINCT Country, COUNT(Country) AS Occurences FROM "Employee" GROUP BY "Country"
+UNION ALL
+SELECT DISTINCT Country, COUNT(Country) AS Occurences FROM "Customer" GROUP BY "Country"
+UNION ALL
+SELECT DISTINCT ShipCountry, COUNT(ShipCountry) AS Occurences FROM "Order" GROUP BY "ShipCountry"
 
-SELECT Country, COUNT(Country) AS Occurences FROM "Employee"
-UNION
-SELECT Country, COUNT(Country) AS Occurences FROM "Customer"
-UNION
-SELECT ShipCountry, COUNT(ShipCountry) AS Occurences FROM "Order"
-
-SELECT DISTINCT Country, COUNT(Country) AS Occurences FROM "Employee" GROUP BY Country
+SELECT Country, AllCountry FROM (
+SELECT Country, COUNT(Country) AS AllCountry FROM "Employee"
+UNION ALL
+SELECT Country, COUNT(Country) AS AllCountry FROM "Customer"
+UNION ALL
+SELECT ShipCountry, COUNT(ShipCountry) AS AllCountry FROM "Order"
+)
+GROUP BY AllCountry
